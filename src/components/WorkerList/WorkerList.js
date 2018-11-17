a1ce89f74f25a007f7a739cc1dd54a8bd5d69812
amalaliza <amalaliza555@gmail.com>
2018-11-17 11:05:29 +0530

    Graphs added

--- src/components/WorkerList/WorkerList.js
+++ src/components/WorkerList/WorkerList.js
@@ -6,26 +6,39 @@ import { workerList } from './WorkList.constants';
 import styles from './WorkerList.css';
 
 class WorkerList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ searchText: e.target.value });
+  }
+
   render() {
+    const { searchText } = this.state;
     return (
       <div className={classNames(mainStyle.sidebar2)}>
         <div className={styles.searchWrapper}>
-          <input type="search" placeholder="search workers" className={styles.input} />
+          <input type="search" placeholder="search workers" className={styles.input} onChange={this.handleChange} />
         </div>
-        {workerList.map(worker => (
+        {workerList.filter(worker => worker.name.includes(searchText)).map(worker => (
           <Avatar
             avatar={worker}
             showIdNumber
           />
         ))}
         <div>
-            <button>Prev</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>Next</button>
+          <button>Prev</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>Next</button>
         </div>
       </div>
     );
